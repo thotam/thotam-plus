83ccda513916f9f0a3b31e8a742a1a885d4efa48
thotam <thanhtamtqno1@gmail.com>
2022-10-05 10:07:46 +0700

    Update BenhVien.php

--- src/Models/BenhVien.php
+++ src/Models/BenhVien.php
@@ -15,65 +15,66 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BenhVien extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
-    use Userstamps;
-    use TinhHuyenXaTrait;
+	use HasFactory;
+	use SoftDeletes;
+	use Userstamps;
+	use TinhHuyenXaTrait;
 
-    /**
-     * Disable Laravel's mass assignment protection
-     *
-     * @var array
-     */
-    protected $guarded = ['id'];
+	/**
+	 * Disable Laravel's mass assignment protection
+	 *
+	 * @var array
+	 */
+	protected $guarded = ['id'];
 
-    /**
-     * The table associated with the model.
-     *
-     * @var string
-     */
-    protected $table = 'benhviens';
+	/**
+	 * The table associated with the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'benhviens';
 
-    /**
-     * Get the hr that owns the BenhVien
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function hr(): BelongsTo
-    {
-        return $this->belongsTo(HR::class, 'hr_key');
-    }
+	/**
+	 * Get the hr that owns the BenhVien
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+	 */
+	public function hr(): BelongsTo
+	{
+		return $this->belongsTo(HR::class, 'hr_key');
+	}
 
-    /**
-     * Get all of the hoithaos for the BenhVien
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\HasMany
-     */
-    public function hoithaos(): HasMany
-    {
-        return $this->hasMany(HoithaoEtcList::class, 'benhvien_id');
-    }
+	/**
+	 * Get all of the hoithaos for the BenhVien
+	 *
+	 * @return \Illuminate\Database\Eloquent\Relations\HasMany
+	 */
+	public function hoithaos(): HasMany
+	{
+		return $this->hasMany(HoithaoEtcList::class, 'benhvien_id');
+	}
 
-    /**
-     * @var array TUYEN
-     */
-    const TUYEN = [
-        '1' => 'Trung ương',
-        '2' => 'Tỉnh',
-        '3' => 'Huyện',
-    ];
+	/**
+	 * @var array TUYEN
+	 */
+	const TUYEN = [
+		'1' => 'Trung ương',
+		'2' => 'Tỉnh',
+		'3' => 'Huyện',
+		'9' => 'Phòng khám Đa khoa',
+	];
 
-    /**
-     * Get the bệnh viện's tuyến.
-     *
-     * @return \Illuminate\Database\Eloquent\Casts\Attribute
-     */
-    protected function tuyen(): Attribute
-    {
-        return Attribute::make(
-            get: function () {
-                return collect(self::TUYEN)->get($this->tuyen_id);
-            },
-        );
-    }
+	/**
+	 * Get the bệnh viện's tuyến.
+	 *
+	 * @return \Illuminate\Database\Eloquent\Casts\Attribute
+	 */
+	protected function tuyen(): Attribute
+	{
+		return Attribute::make(
+			get: function () {
+				return collect(self::TUYEN)->get($this->tuyen_id);
+			},
+		);
+	}
 }
